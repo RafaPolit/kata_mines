9ca0f8a782444b23400d1a158d08ed990448043c
RafaPolit <rafaelpolit@gmail.com>
2016-10-28 16:42:39 -0500

    Kata resolved

--- client/app/scripts/mines/controller.js
+++ client/app/scripts/mines/controller.js
@@ -1,15 +1,54 @@
 'use strict';
 
 angular.module('Minesweeper')
-.controller('minesweeper', function ($scope) {
+.controller('minesweeper', function ($scope, initialState) {
+  initialState = initialState || [];
+  $scope.state = [];
+  $scope.clickedBoxes = [];
+  $scope.bombClicked = false;
 
-  $scope.controller_loaded = 'Minesweeper loaded!';
+  initialState.forEach(function(row) {
+    var colData = [];
+    for (var col = 0; col < initialState.length; col ++) {
+      colData.push(row[col] === '*' ? 'Bomb!' : 0);
+    }
+    $scope.state.push(colData);
+    $scope.clickedBoxes.push(colData.map(function() { return false; }));
+  });
 
+  $scope.state = $scope.state.map(function(row, rowIndex) {
+    return row.map(function(col, colIndex) {
+      if (col === 'Bomb!') { return col; }
+      return getSurroundingBombs($scope.state, row, col, rowIndex, colIndex);
+    });
+  });
+
+  $scope.clickCell = function(row, col) {
+    $scope.clickedBoxes[row][col] = true;
+    $scope.bombClicked = $scope.state[row][col] === 'Bomb!';
+  }
+  // ---
+
+  function getSurroundingBombs(initialState, row, col, rowIndex, colIndex) {
+    var surroundingBombs = 0;
+    if (rowIndex > 0 && colIndex > 0 && initialState[rowIndex - 1][colIndex -1] === 'Bomb!') { surroundingBombs++; }
+    if (rowIndex > 0 && initialState[rowIndex - 1][colIndex] === 'Bomb!') { surroundingBombs++; }
+    if (rowIndex > 0 && initialState[rowIndex - 1][colIndex + 1] === 'Bomb!') { surroundingBombs++; }
+    if (colIndex > 0 && initialState[rowIndex][colIndex - 1] === 'Bomb!') { surroundingBombs++; }
+    if (initialState[rowIndex][colIndex + 1] === 'Bomb!') { surroundingBombs++; }
+    if (rowIndex < initialState.length - 1 && colIndex > 0 && initialState[rowIndex + 1][colIndex - 1] === 'Bomb!') { surroundingBombs++; }
+    if (rowIndex < initialState.length - 1  && initialState[rowIndex + 1][colIndex] === 'Bomb!') { surroundingBombs++; }
+    if (rowIndex < initialState.length - 1 && initialState[rowIndex + 1][colIndex + 1] === 'Bomb!') { surroundingBombs++; }
+    return (surroundingBombs);
+  }
 })
 .config(function ($routeProvider) {
   $routeProvider
   .when('/minesweeper', {
     templateUrl: 'scripts/mines/views/minesweeper.html',
-    controller: 'minesweeper'
+    controller: 'minesweeper',
+    resolve: {
+      initialState: function() { return ['*....', '.....', '.*...', '.****']; }
+    }
   });
 });
--- client/app/scripts/mines/specs/controller_spec.js
+++ client/app/scripts/mines/specs/controller_spec.js
@@ -7,16 +7,53 @@ describe('Controller: minesweeper', function () {
 
   var controller;
   var scope;
+  var initialState
 
-  beforeEach(inject(function ($rootScope, $controller) {
+  function instantiateController() {
+    inject(function ($controller) {
+      controller = $controller('minesweeper', { $scope: scope, initialState: initialState });
+    });
+  }
+
+  beforeEach(inject(function ($rootScope) {
     scope = $rootScope.$new();
-    controller = $controller('minesweeper', { $scope: scope });
+    instantiateController();
   }));
 
   describe('On instance', function () {
-    it('should set "controller_loaded" variable in scope', function () {
-      expect(scope.controller_loaded).toContain('loaded');
+    it('should put into scope the initial state', function () {
+      initialState = ['*..', '...', '.*.'];
+      instantiateController();
+      expect(scope.state.length).toBe(3);
+      expect(scope.state).toEqual([['Bomb!', 1, 0], [2, 2, 1], [1, 'Bomb!', 1]]);
     });
+
+    it('should initiate an empty array of clicked boxes', function() {
+      expect(scope.clickedBoxes).toEqual([[false, false, false], [false, false, false], [false, false, false]]);
+    })
+  });
+
+  describe('clickCell', function() {
+    it('should change the state of the cell', function() {
+      scope.clickCell(1,2);
+      expect(scope.clickedBoxes[1][2]).toBe(true);
+      scope.clickCell(2,1);
+      expect(scope.clickedBoxes[2][1]).toBe(true);
+    });
+
+    it('should not return a clicked cel to false', function() {
+      scope.clickCell(1,2);
+      scope.clickCell(1,2);
+      expect(scope.clickedBoxes[1][2]).toBe(true);
+    });
+
+    it('should set the bombClicked state to true', function() {
+      expect(scope.bombClicked).toBe(false);
+      scope.clickCell(0,1);
+      expect(scope.bombClicked).toBe(false);
+      scope.clickCell(0,0);
+      expect(scope.bombClicked).toBe(true);
+    })
   });
 
   describe('when going to /minesweeper', function () {
