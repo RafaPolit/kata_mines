95e0322918ad78d698c4a69e472e2ec5fc3e955d
RafaPolit <rafaelpolit@gmail.com>
2016-10-28 20:15:45 -0500

    Further refactor.

--- client/app/scripts/mines/controller.js
+++ client/app/scripts/mines/controller.js
@@ -13,7 +13,7 @@ angular.module('Minesweeper')
 
   $scope.state = $scope.state.map(function(row, rowIndex) {
     return row.map(function(col, colIndex) {
-      return col === 'Bomb!' ? col : getSurroundingBombs($scope.state, row, col, rowIndex, colIndex);
+      return col === 'Bomb!' ? col : getSurroundingBombs(rowIndex, colIndex);
     });
   });
 
@@ -22,8 +22,8 @@ angular.module('Minesweeper')
     $scope.bombClicked = $scope.bombClicked || $scope.state[row][col] === 'Bomb!';
   }
 
-  function getSurroundingBombs(state, row, col, rowIndex, colIndex) {
-    return [].concat.apply([], state
+  function getSurroundingBombs(rowIndex, colIndex) {
+    return [].concat.apply([], $scope.state
     .slice(Math.max(rowIndex - 1, 0), rowIndex + 2)
     .map(function(row) { return row.slice(Math.max(colIndex - 1, 0), colIndex + 2)}))
     .filter(function(cell) { return cell === 'Bomb!'; }).length;
@@ -35,7 +35,7 @@ angular.module('Minesweeper')
     templateUrl: 'scripts/mines/views/minesweeper.html',
     controller: 'minesweeper',
     resolve: {
-      initialState: function() { return ['*....', '.....', '.*...', '.****']; }
+      initialState: function() { return ['*....*', '......', '.*....', '.****.', '.****.']; }
     }
   });
 });
